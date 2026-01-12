ee5449346fa33c915e318b671a973ae448b48883
DavinciDreams <l.m.watts3@gmail.com>
2026-01-12 07:38:38 -0500

    remove wallet

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,5 +1,3 @@
-import { Web3Provider } from "@ethersproject/providers"
-import { Web3ReactProvider } from "@web3-react/core"
 import React, { Suspense } from "react"
 import ReactDOM from "react-dom/client"
 import { AudioProvider } from "./context/AudioContext"
@@ -7,8 +5,6 @@ import { AudioProvider } from "./context/AudioContext"
 import { AccountProvider } from "./context/AccountContext"
 import { SceneProvider } from "./context/SceneContext"
 import { ViewProvider } from "./context/ViewContext"
-import { WalletSelectorContextProvider } from './context/WalletSelectorContext'
-import "@near-wallet-selector/modal-ui/styles.css";
 
 
 
@@ -19,17 +15,15 @@ import App from "./components/App"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <WalletSelectorContextProvider>
-      <AudioProvider>
-        <ViewProvider>
-          <SceneProvider>
-          <LoadingOverlay />
-            <Suspense>
-              <App />
-            </Suspense>
-          </SceneProvider>
-        </ViewProvider>
-      </AudioProvider>
-    </WalletSelectorContextProvider>
+    <AudioProvider>
+      <ViewProvider>
+        <SceneProvider>
+        <LoadingOverlay />
+          <Suspense>
+            <App />
+          </Suspense>
+        </SceneProvider>
+      </ViewProvider>
+    </AudioProvider>
   </React.StrictMode>,
 )
