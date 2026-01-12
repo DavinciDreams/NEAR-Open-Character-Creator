6d0ebc2dd604ce68bd8c2b78ac3004a653e782c0
DavinciDreams <l.m.watts3@gmail.com>
2026-01-12 07:47:11 -0500

    no mint

--- src/constants/address.js
+++ src/constants/address.js
@@ -1,4 +1,2 @@
-// export const NFT_CONTRACT_IDS = [ "chedera.testnet", "shedera.testnet", "nhedera.testnet" ]; 
-export const NFT_CONTRACT_ID = "unicorndev99.testnet";
-// export const NFT_CONTRACT_ID = "atlasavatar99.testnet";
+// export const NFT_CONTRACT_ID = "testnet";
 
