fd1fa31219df8d5783065725fec075edf63669f8
DavinciDreams <l.m.watts3@gmail.com>
2026-01-12 07:44:49 -0500

    vite port

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,16 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps : {
-    disabled : false
+  server: {
+    port: 5174
   },
-  build : {
-    commonjsOptions : {
-      include : []
+  // optimizeDeps.disabled removed for Vite 5.1+ compatibility
+  build: {
+    commonjsOptions: {
+      include: ['bn.js'],
+      namedExports: {
+        'bn.js': ['BN']
+      }
     }
   }
 })
